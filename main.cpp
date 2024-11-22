369a8ba4a0025d84997f61e40f365c777ccfecc7
Khazanovich Lev <levkhazanovich335@gmail.com>
2024-11-22 15:04:42 +0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+void fileInput(){
+
+}
+void fileOutput(){
+
+}
+
+void print(){
+
+}
+
+int main()
+{
+
+
+    return 0;
+}
